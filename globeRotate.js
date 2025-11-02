6779dce3977e5e32e824518e0d60c63411d24355
popthl <52552774+popthl@users.noreply.github.com>
2025-11-03 07:07:03 +0800

    Implement GlobeRotate class for camera control

---
+++ globeRotate.js
@@ -0,0 +1,37 @@
+class GlobeRotate {
+   constructor(viewer) {
+       this._viewer = viewer;
+   }
+   _icrf() {
+       if (this._viewer.scene.mode !== Cesium.SceneMode.SCENE3D) {
+           return true;
+       }
+       let icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix(this._viewer.clock.currentTime);
+       if (icrfToFixed) {
+           let camera = this._viewer.camera;
+           let offset = Cesium.Cartesian3.clone(camera.position);
+           let transform = Cesium.Matrix4.fromRotationTranslation(icrfToFixed);
+           camera.lookAtTransform(transform, offset);
+       }
+   }
+   _bindEvent() {
+       //this._viewer.clock.multiplier = 15;
+       //this._viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+       this._viewer.scene.postUpdate.addEventListener(this._icrf, this);
+   }
+   _unbindEvent() {
+       //this._viewer.clock.multiplier = 1;
+       //this._viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+       this._viewer.scene.postUpdate.removeEventListener(this._icrf, this);
+   }
+   start() {
+       this._viewer.clock.shouldAnimate = true;
+       this._unbindEvent();
+       this._bindEvent();
+       return this;
+   }
+   stop() {
+       this._unbindEvent();
+       return this;
+   }
+}
